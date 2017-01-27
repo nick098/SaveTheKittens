63f485dd9e848f65db61245642f134bef75276f6
Nicholas Brown <nick098@brownsrus.net>
2017-01-26 21:09:40 -0600

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Nicholas Brown on 1/26/17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
